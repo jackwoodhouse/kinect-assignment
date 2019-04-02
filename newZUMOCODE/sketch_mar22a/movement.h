92a796601a24d31f2235ddaf21df2150936c3f2e
6c69616d <44211257+6c69616d@users.noreply.github.com>
2019-04-02 10:09:45 +0100

    Comment and slightly adapt the movement controls

--- newZUMOCODE/sketch_mar22a/movement.h
+++ newZUMOCODE/sketch_mar22a/movement.h
@@ -1,32 +1,48 @@
-#include <ZumoShield.h>
+#include <ZumoShield.h> // import the ZumoShield header file
 
-class Movement{
+class Movement { // Movement class declaration
 
-    private:
-    static ZumoMotors motors; //declare a variable called motors of type ZumoMotors so that we can set the speeds of the motors to control the robot
-                            // static because can't call non static variable from inside static method
+  private:
+    static ZumoMotors motors; //create an instance of the ZumoMotors class so that we can set the speeds of the motors to control the robot
+    // static because can't call non static variable from inside static method
 
-    public:
-    // static methods so I didn't need to istaniate the class
+  public:
+    // static methods so we didn't need to istaniate the class
     static void forward(int speed) // method for the forward movement
     {
-        motors.setSpeeds(speed, speed); // set both motors to the speed passed in
+      restrictSpeedIfNeeded(speed); // call the restrict speed if needed method
+      motors.setSpeeds(speed, speed); // set both the motor speeds to the value of speed
     };
     static void backward(int speed) // method for the reverse movement
     {
-        motors.setSpeeds(-speed, -speed); // set both motors in reverse to the speed passed in
+      restrictSpeedIfNeeded(speed); // call the restrict speed if needed method
+      motors.setSpeeds(-speed, -speed); // set both the motor speeds in reverse to the value of speed
     };
     static void left(int speed) // method for moving the Zumo left
     {
-        motors.setSpeeds(-speed, speed); // set the left motor in reverse to the speed passed and the right motor to the speed passed in forwards
+      restrictSpeedIfNeeded(speed); // call the restrict speed if needed method
+      motors.setSpeeds(-speed, speed); // set the left motor in reverse to the value of speedand the right motor to the value of speed in forwards
+      delay(890); // to prevent the Zumo turning more than 90 degrees
+      halt(); // stop the Zumo
     };
     static void right(int speed) // method for moving the Zumo right
     {
-        motors.setSpeeds(speed, -speed); // set the right motor in reverse to the speed passed and the left motor to the speed passed in forwards
+      restrictSpeedIfNeeded(speed); // call the restrict speed if needed method
+      motors.setSpeeds(speed, -speed); // set the right motor in reverse to the value of speed and the left motor to the value of speed in forwards
+      delay(890); // to prevent the Zumo from turning more than 90 degrees
+      halt(); // stop the Zumo
     };
     static void halt() // method making the Zumo come to a stop
     {
-        motors.setSpeeds(0,0); // set both the motors to a speed of Zumo
+      motors.setSpeeds(0, 0); // set both the motors to a speed of 0
+    };
+    static int restrictSpeedIfNeeded(int speed) // restrict the speed to prevent damaging the Zumo motors
+    {
+      if (speed > 125) // if the value of speed is greater than 125
+      {
+        speed = 125; // set speed to 125
+      }
+      return speed; // return the speed of either the orginial speed passed in or 125
     };
 
 };
